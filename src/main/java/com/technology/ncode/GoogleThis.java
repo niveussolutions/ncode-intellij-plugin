c99ec5a032933c4206996f2eeabe9aa90fc8a7d0
Shreyas Prabhu <shreyas.int@niveussolutions.com>
2025-02-06 12:21:47 +0530

    Initial commit

---
+++ src/main/java/com/technology/ncode/GoogleThis.java
@@ -0,0 +1,25 @@
+package com.technology.ncode;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Editor;
+import org.jetbrains.annotations.NotNull;
+
+public class GoogleThis extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+            final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+            final CaretModel caretModel = editor.getCaretModel();
+            String selectedText = caretModel.getCurrentCaret().getSelectedText();
+            BrowserUtil.browse("https://www.google.com/search?q="+selectedText);
+        }
+    @Override
+    public void update(AnActionEvent e) {
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final CaretModel caretModel = editor.getCaretModel();
+        e.getPresentation().setEnabledAndVisible(caretModel.getCurrentCaret().hasSelection());
+    }
+}
